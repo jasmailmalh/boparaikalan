354fd6eecdd5869668e96960a47d2ba74b0dd176
sgpc <jasmailmalh13@gmail.com>
2023-11-25 12:15:38 +0530

    remove php code

--- index.php
+++ index.php
@@ -9,10 +9,6 @@
   <body>
     <h1>Waheguru ji ka khalsa <br>
       Waheguru ji ki fateh </h1>
-
-      <?php
-      echo  "Hello World" ; 
-      ?>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
